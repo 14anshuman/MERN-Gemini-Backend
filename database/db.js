30ae8a723a51c0a95d5cef5ddac624e311d3b9a2
14anshuman <singhanshuman8182@gmail.com>
2025-05-03 20:05:36 +0530

    db.js

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.Db_URL, {
+    await mongoose.connect(process.env.DB_URL, {
       dbName: "ChatbotYoutube",
     });
 
